8d01cf99c33ce4df3d3d645f44af30611bd577c9
GiuseppeBergamo <giuseppe.bergamo67@gmail.com>
2022-09-06 17:34:21 +0200

    group by query 1

--- test.sql
+++ test.sql
@@ -37,6 +37,7 @@ SELECT * FROM `teachers` WHERE `phone` IS NULL;
 
 -- 1. Contare quanti iscritti ci sono stati ogni anno
 
+SELECT COUNT(*) AS `studenti`, YEAR(`enrolment_date`) FROM `students` GROUP BY YEAR(`enrolment_date`); 
 
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
 
